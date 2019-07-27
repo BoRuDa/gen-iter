c5785fdba37adcad830e3888a07db2303ca5e9f6
Bogdan <hide>
2019-07-27 15:24:07 +0200

    Fix typos.

--- usage.go
+++ usage.go
@@ -1,13 +1,13 @@
 package main
 
-const usage = `gen-iter is open source tool for generating Iterators in Go.
+const usage = `gen-iter is an open source tool for generating iterators in Go.
 
 	Usage:
 		gen-iter [flags]
 
 	Flags:
 		-t, -type		type of Iterator
-		-p, -pkg		name of package for generated go-file
-		-d, -dir		directory for generated Iterator [optional]
+		-p, -pkg		name of a package for generated go-file
+		-d, -dir		directory for a generated Iterator [optional]
 
 `
